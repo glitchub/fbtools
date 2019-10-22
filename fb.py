bc20ec80b70dadf8057ed4dcae48ab708e0c6854
glitchub <glitchub@users.noreply.github.com>
2019-10-22 12:28:25 -0700

    Merge modules to fb.py

--- fb.py
+++ fb.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python2
 
+from __future__ import print_function
 import os, sys
 from ctypes import *
+from pgmagick import *
 
+# directory containing this module also contains fb.bin and needed fonts
 __here__=os.path.dirname(__file__) or '.'
 
-class framebuffer(object):
+# create a frame buffer object which interfaces with library fb.bin
+class framebuffer():
     # open indexed framebuffer and mmap it
     def __init__(self, device="/dev/fb0"):
 
@@ -34,3 +38,160 @@ class framebuffer(object):
         pixels=len(rgb)/3
         if self.lib.fbunpack(byref(self.fbinfo), pixels, 0, rgb):
             raise Exception("fbunpack %d pixels failed" % pixels)
+
+# Clear framebuffer
+# color: the color to fill
+# device: the name of the frame buffer device
+def fbclear(color="black", device="/dev/fb0"):
+    fb=framebuffer(device)
+    im=Image(Geometry(fb.width, fb.height), Color(color))
+    blob=Blob()
+    im.write(blob,"RGB",8)
+    fb.pack(blob.data)
+
+# Write image file to framebuffer. Most graphic images formats are supported
+# (via imagemagick). If '-' then image is on stdin. Image will be scaled automatically to fit.
+# margin: size in pixels of display margins
+# stretch: if True, then aspect ratio is not maintained, image is stretch to fit completely within margins
+# color: color of any unrendered background
+# device: name of the frame buffer device
+# debug,: if True, print some diagnostic info
+def fbimage(image, margin=0, stretch=False, color="black", device="/dev/fb0", debug=False):
+
+    input = sys.stdin if image == '-' else open(image)
+
+    # open framebuffer so we can get its geometry
+    fb=framebuffer(device)
+    if debug: print("frame buffer height=%d width=%d" % (fb.height, fb.width))
+
+    # create background layer
+    base=Image(Geometry(fb.width, fb.height), Color(color))
+    base.fillColor(Color(color))
+    base.strokeWidth(0)
+
+    im=Image(Blob(input.read()))
+    if debug: print("original image height=%d width=%d" % (im.rows(), im.columns()))
+
+    g=Geometry(fb.width-(margin*2), fb.height-(margin*2))
+    if stretch: g.aspect(True)
+    im.scale(g)
+    if debug: print("scaled image height=%d width=%d" % (im.rows(), im.columns()))
+
+    base.composite(im, GravityType.CenterGravity, CompositeOperator.OverCompositeOp)
+
+    blob=Blob()
+    base.write(blob,"RGB",8)
+    fb.pack(blob.data)
+
+# Display arbitrary text in frame buffer.
+# input: a list of one or more lines of text, or the name of a text file ('-' for stdin)
+# fg: text and border color.
+# bg: background color.
+# gravity: display alignment, one of "nw", "n", "ne", "w", "c", "e", "sw", "s", or "se"
+# font: path to a ttf file, default is a monospaced sans-serif with Chinese characters
+# margin: display edge to avoid
+# border: width in pixels of screen border, in the fg color
+# wrap: if True, wrap tjhe text to fit within margins, requires monospace font
+# xtrim: if True, chop long lines to fit within horizontal margins, requires monospace font
+# ytrim: if True, chop text lines to fit within vertical margins
+# reference: font's widest character, required for wrap and xtrim
+# device: name of the framebuffer device
+# debug: print some debug data, don't set this
+def fbtext(input, fg="White", bg="black", gravity='nw',point=20,
+           font=__here__+"/WenQuanYiMicroHeiMono.ttf", device="/dev/fb0",
+           margin=10, border=0, wrap=False, xtrim=False, ytrim=False,
+           reference="M", debug=False):
+
+    if type(input) is list:
+        # clean up the list
+        text = map(lambda s:s.expandtabs().decode("utf8"),input)
+    else:
+        fh = sys.stdin if input == '-' else open(input)
+        text = map(lambda s:s.expandtabs(),fh.read().decode("utf8").splitlines())
+
+    fb = framebuffer(device)
+    if debug: print("frame buffer height=%d width=%d" % (fb.height, fb.width))
+
+    # create surface
+    im = Image(Geometry(fb.width,fb.height),Color(bg or "black"))
+    im.fillColor(Color(fg or "white"))
+    im.strokeWidth(0)
+    im.font(font)
+    im.fontPointsize(point)
+
+    if border == 1:
+        im.draw(DrawableLine(0, 0, fb.width-1, 0)) # across the top
+        im.draw(DrawableLine(0, 0, 0, fb.height-1)) # down the left
+        im.draw(DrawableLine(fb.width-1, 0, fb.width-1, fb.height-1)) # down the right
+        im.draw(DrawableLine(0, fb.height-1, fb.width-1, fb.height-1)) # across the bottom
+    elif border > 0:
+        im.draw(DrawableRectangle(0, 0, fb.width-1, border-1)) # across the top
+        im.draw(DrawableRectangle(0, 0, border-1, fb.height-1)) # down the left
+        im.draw(DrawableRectangle(fb.width-border, 0, fb.width-1, fb.height-1)) # down the right
+        im.draw(DrawableRectangle(0, fb.height-border, fb.width-1, fb.height-1)) # across the bottom
+
+    margin += border
+
+    # get font info
+    tm = TypeMetric()
+    im.fontTypeMetrics(reference,tm)
+    if debug:
+        print("reference char '%s': ascent=%d descent=%d textheight=%d textwidth=%d maxadvance=%d" %
+            (reference, tm.ascent(),tm.descent(),tm.textHeight(),tm.textWidth(), tm.maxHorizontalAdvance()))
+
+    # x and y margins are based on gravity
+    if gravity in ("nw","ne","w","e","sw","se"):
+        xoffset = margin
+    else:
+        xoffset = 0
+
+    if gravity in ("nw","n","ne"):
+        yoffset = margin+tm.ascent()
+    elif gravity in ("sw","s","se"):
+        yoffset = margin+(-tm.descent())
+    else:
+        yoffset=0
+
+    maxcols = int((fb.width-(margin*2))/tm.textWidth())
+    maxlines = int((fb.height-(margin*2))/(tm.textHeight()+1))
+    if debug: print("xoffset=%d yoffset=%d maxcols=%d maxlines=%d" % (xoffset, yoffset, maxcols, maxlines))
+
+    if wrap:
+        def wrapt(t):
+            t = t.rstrip(' ')
+            while len(t) > maxcols:
+                i = t[:maxcols].rfind(' ')
+                if (i < 0):
+                    yield t[:maxcols]
+                    t = t[maxcols:]
+                else:
+                    yield t[:i]
+                    t = t[i+1:]
+                t = t.rstrip(' ')
+            yield t
+        text = [w for t in text for w in wrapt(t)]
+
+    if ytrim:
+        text = text[:maxlines]
+
+    if xtrim:
+        text = map(lambda s:s[:maxcols],text)
+
+    if debug: print('\n'.join(text))
+
+    dl = DrawableList()
+    dl.append(DrawableGravity({"nw": GravityType.NorthWestGravity,
+                               "n":  GravityType.NorthGravity,
+                               "ne": GravityType.NorthEastGravity,
+                               "w":  GravityType.WestGravity,
+                               "c":  GravityType.CenterGravity,
+                               "e":  GravityType.EastGravity,
+                               "sw": GravityType.SouthWestGravity,
+                               "s":  GravityType.SouthGravity,
+                               "se": GravityType.SouthEastGravity}[gravity]))
+    dl.append(DrawableText(xoffset, yoffset, '\n'.join(text).encode("utf8")))
+    im.draw(dl)
+
+    blob = Blob()
+    im.write(blob,"RGB",8)
+    fb.pack(blob.data)
