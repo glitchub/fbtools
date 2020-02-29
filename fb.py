3d0e5c9292338ac9a2a748c3ee1d15414251cc4f
Rich Leggitt <glitchub@users.noreply.github.com>
2020-02-28 16:50:54 -0800

    Default to python3 (but python2 still works)

--- fb.py
+++ fb.py
@@ -1,12 +1,25 @@
-#!/usr/bin/python2
+# python interface to pgmagick and fb.bin, works with python 2 or 3
 
-from __future__ import print_function
+from __future__ import print_function, division
 import os, sys
 from ctypes import *
 from pgmagick import *
 
 # directory containing this module also contains fb.bin and needed fonts
 __here__=os.path.dirname(__file__) or '.'
+
+# python version-agnostic convert object to str or bytes
+def _to_str(s):
+    try:
+        return s.decode('utf-8')
+    except:
+        return str(s)
+
+def _to_bytes(s):
+    try:
+        return bytes(s, 'utf-8')
+    except:
+        return bytes(s)
 
 # create a frame buffer object which interfaces with library fb.bin
 class framebuffer():
@@ -23,21 +36,23 @@ class framebuffer():
             quit(1)
 
         self.fbinfo=(c_uint*16)(); # too big is ok
-        res=self.lib.fbopen(byref(self.fbinfo), device)
+        res=self.lib.fbopen(byref(self.fbinfo), _to_bytes(device))
         if res:
             raise Exception("fbopen %s failed (%d)" % (device, res))
         self.height=int(self.fbinfo[0])
         self.width=int(self.fbinfo[1])
 
     def pack(self, rgb):
-        pixels=len(rgb)/3
+        rgb=_to_bytes(rgb)
+        pixels=len(rgb)//3
         if self.lib.fbpack(byref(self.fbinfo), pixels, 0, rgb):
             raise Exception("fbpack %d pixels failed" % pixels)
 
-    def unpack(self, rgb):
-        pixels=len(rgb)/3
+    def unpack(self, pixels):
+        rgb=create_string_buffer(pixels*3)
         if self.lib.fbunpack(byref(self.fbinfo), pixels, 0, rgb):
             raise Exception("fbunpack %d pixels failed" % pixels)
+        return _to_str(rgb)
 
 # Clear framebuffer
 # color: the color to fill
@@ -58,7 +73,11 @@ def fbclear(color="black", device="/dev/fb0"):
 # debug,: if True, print some diagnostic info
 def fbimage(image, margin=0, stretch=False, color="black", device="/dev/fb0", debug=False):
 
-    input = sys.stdin if image == '-' else open(image)
+    if image == '-':
+        try: fh = sys.stdin.buffer # python 3
+        except: fh = sys.stdin     # python 2
+    else:
+        fh = open(image, mode='rb')
 
     # open framebuffer so we can get its geometry
     fb=framebuffer(device)
@@ -69,7 +88,7 @@ def fbimage(image, margin=0, stretch=False, color="black", device="/dev/fb0", de
     base.fillColor(Color(color))
     base.strokeWidth(0)
 
-    im=Image(Blob(input.read()))
+    im=Image(Blob(fh.read()))
     if debug: print("original image height=%d width=%d" % (im.rows(), im.columns()))
 
     g=Geometry(fb.width-(margin*2), fb.height-(margin*2))
@@ -104,10 +123,15 @@ def fbtext(input, fg="White", bg="black", gravity='nw',point=20,
 
     if type(input) is list:
         # clean up the list
-        text = map(lambda s:s.expandtabs().decode("utf8"),input)
+        text = map(lambda s:_to_str(s).expandtabs(),input)
     else:
-        fh = sys.stdin if input == '-' else open(input)
-        text = map(lambda s:s.expandtabs(),fh.read().decode("utf8").splitlines())
+        if input == '-':
+            try: fh = sys.stdin.buffer # python 3
+            except: fh = sys.stdin     # python 2
+        else:
+            fh = open(input, mode='rb')
+
+        text = map(lambda s:s.expandtabs(),_to_str(fh.read()).splitlines())
 
     fb = framebuffer(device)
     if debug: print("frame buffer height=%d width=%d" % (fb.height, fb.width))
@@ -152,8 +176,8 @@ def fbtext(input, fg="White", bg="black", gravity='nw',point=20,
     else:
         yoffset=0
 
-    maxcols = int((fb.width-(margin*2))/tm.textWidth())
-    maxlines = int((fb.height-(margin*2))/(tm.textHeight()+1))
+    maxcols = int((fb.width-(margin*2))//tm.textWidth())
+    maxlines = int((fb.height-(margin*2))//(tm.textHeight()+1))
     if debug: print("xoffset=%d yoffset=%d maxcols=%d maxlines=%d" % (xoffset, yoffset, maxcols, maxlines))
 
     if wrap:
@@ -189,7 +213,7 @@ def fbtext(input, fg="White", bg="black", gravity='nw',point=20,
                                "sw": GravityType.SouthWestGravity,
                                "s":  GravityType.SouthGravity,
                                "se": GravityType.SouthEastGravity}[gravity]))
-    dl.append(DrawableText(xoffset, yoffset, '\n'.join(text).encode("utf8")))
+    dl.append(DrawableText(xoffset, yoffset, _to_bytes('\n'.join(text))))
     im.draw(dl)
 
     blob = Blob()
